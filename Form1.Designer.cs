7e588838a66fef9860ac2ebe0dcaf1a3993effa9
Olga Bachurina <bachuri12@ukr.net>
2024-02-15 18:01:25 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsAppPractic1ex6SP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownStart = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownEnd = new System.Windows.Forms.NumericUpDown();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDownThreads = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreads)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownStart
+            // 
+            this.numericUpDownStart.Location = new System.Drawing.Point(97, 42);
+            this.numericUpDownStart.Name = "numericUpDownStart";
+            this.numericUpDownStart.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownStart.TabIndex = 0;
+            // 
+            // numericUpDownEnd
+            // 
+            this.numericUpDownEnd.Location = new System.Drawing.Point(412, 42);
+            this.numericUpDownEnd.Name = "numericUpDownEnd";
+            this.numericUpDownEnd.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownEnd.TabIndex = 1;
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Red;
+            this.StartButton.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StartButton.Location = new System.Drawing.Point(260, 155);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(125, 48);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Cтарт";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(221, 228);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 129);
+            this.textBox1.TabIndex = 3;
+            // 
+            // numericUpDownThreads
+            // 
+            this.numericUpDownThreads.Location = new System.Drawing.Point(260, 116);
+            this.numericUpDownThreads.Name = "numericUpDownThreads";
+            this.numericUpDownThreads.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownThreads.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Salmon;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите начало диапазона";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Salmon;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(364, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите конец диапазона";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Salmon;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(193, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите количество потоков";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(660, 381);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownThreads);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.numericUpDownEnd);
+            this.Controls.Add(this.numericUpDownStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreads)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownStart;
+        private System.Windows.Forms.NumericUpDown numericUpDownEnd;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownThreads;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace WindowsFormsAppPractic1ex6SP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int startRange = (int)numericUpDownStart.Value;
+            int endRange = (int)numericUpDownEnd.Value;
+            int numberOfThreads = (int)numericUpDownThreads.Value;
+            int range = endRange - startRange + 1;
+            int rangePerThread = range / numberOfThreads;
+            var tasks = new List<Task>();
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                int start = startRange + i * rangePerThread;
+                int end = (i == numberOfThreads - 1) ? endRange : start + rangePerThread - 1;
+                tasks.Add(Task.Run(() => DisplayNumbers(start, end)));
+            }
+            await Task.WhenAll(tasks);
+            MessageBox.Show("Все потоки завершили выполнение");
+        }
+        private void DisplayNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                int number = i; 
+                textBox1.Invoke((MethodInvoker)delegate
+                {
+                    textBox1.AppendText(number + Environment.NewLine);
+                });
+                Task.Delay(5).Wait(); 
+            }
+        }
+    }
+}
+
+
